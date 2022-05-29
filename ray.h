15209905decec192eb078144ae74f65613c30fac
Trevor <trevllew751@vt.edu>
2022-05-28 17:35:53 -0700

    Created the ray class

---
+++ ray.h
@@ -0,0 +1,30 @@
+//
+// Created by Trevl on 5/28/2022.
+//
+
+#ifndef RAYTRACING_RAY_H
+#define RAYTRACING_RAY_H
+
+#include "vec3.h"
+
+class ray {
+public:
+
+    point3 orig;
+    vec3 dir;
+
+public:
+
+    ray() {}
+    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+
+    point3 origin() const { return orig; }
+    vec3 direction() const  { return dir; }
+
+    point3 at(double t) {
+        return orig + (t * dir);
+    }
+
+};
+
+#endif //RAYTRACING_RAY_H
