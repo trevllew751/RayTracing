05d006c70280a86eb61549b42a03a25b73fa4f7a
Trevor <trevllew751@vt.edu>
2022-06-01 20:20:43 -0700

    Added a sphere to the image

--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@
 #include "color.h"
 #include "ray.h"
 
+
+bool hit_sphere(const point3 &center, double radius, const ray &r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 color ray_color(const ray &r) {
+    if (hit_sphere(point3(0, 0, 1), 0.5, r)) {
+        return {1, 0, 0};
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
@@ -14,7 +27,7 @@ int main() {
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
-    const int image_height = static_cast<int>(image_width/aspect_ratio);
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     // Camera
 
@@ -22,10 +35,10 @@ int main() {
     double viewport_width = aspect_ratio * viewport_height;
     double focal_length = 1.0;
 
-    point3 origin = point3(0,0,0);
+    point3 origin = point3(0, 0, 0);
     vec3 horizontal = vec3(viewport_width, 0, 0);
     vec3 vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -36,7 +49,7 @@ int main() {
         for (int j = 0; j < image_width; j++) {
             auto u = double(j) / (image_width - 1);
             auto v = double(i) / (image_height - 1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
             color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
