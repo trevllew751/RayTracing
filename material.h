180e39d8eaeebcf946915a12a2feeb353b45c547
Trevor <trevorllew@simpleworks.tech>
2023-02-03 17:06:11 -0500

    Created lambertian material class

--- material.h
+++ material.h
@@ -14,4 +14,19 @@ public:
     virtual bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const = 0;
 };
 
+class lambertian : public material {
+public:
+    color albedo;
+
+public:
+    lambertian(const color &a) : albedo(a) {}
+
+    virtual bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override {
+        auto scatter_direction = rec.normal + random_unit_vector();
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
+    }
+};
+
 #endif //RAYTRACING_MATERIAL_H
