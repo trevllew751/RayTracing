78982e82ab707257ab8fe6ff98e618eb1e7b6cbe
Trevor <trevorllew@simpleworks.tech>
2023-02-02 22:39:31 -0500

    Implemented gamma correction with gamma 2

--- color.h
+++ color.h
@@ -14,9 +14,9 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
 
     // Divide the color by the number of samples
     auto scale = 1.0 / samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = std::sqrt(scale * r);
+    g = std::sqrt(scale * g);
+    b = std::sqrt(scale * b);
 
     // Write the translated [0,255] color pixel color
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << " "
