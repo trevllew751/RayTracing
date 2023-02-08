e39c6af1df5a26e3535573cb20a5648398998587
Trevor <trevorllew@simpleworks.tech>
2023-02-08 17:03:12 -0500

    Glass only sometimes refracts

--- material.h
+++ material.h
@@ -51,6 +51,9 @@ public:
 
 class dielectric : public material {
 public:
+    double ir; // Index of Refraction
+
+public:
     dielectric(double index_of_refraction) : ir(index_of_refraction) {}
 
     virtual bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override {
@@ -58,14 +61,20 @@ public:
         double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
 
         vec3 unit_direction = unit_vector(r_in.direction());
-        vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
+        double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
 
-        scattered = ray(rec.p, refracted);
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        vec3 direction;
+
+        if (cannot_refract)
+            direction = reflect(unit_direction, rec.normal);
+        else
+            direction = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, direction);
         return true;
     }
-
-public:
-    double ir; // Index of Refraction
 };
 
 #endif //RAYTRACING_MATERIAL_H
