54b596e28ca2d967b1e887caf6bb8cf6708b1751
Trevor <trevorllew@simpleworks.tech>
2023-02-08 16:08:39 -0500

    Created shiny metal reflection

--- main.cpp
+++ main.cpp
@@ -4,10 +4,9 @@
 #include "hittable_list.h"
 #include "sphere.h"
 #include "camera.h"
+#include "material.h"
 
 #include <iostream>
-
-#define LAMBERTIAN 0
 
 color ray_color(const ray &r, const hittable &world, int depth) {
     hit_record rec;
@@ -18,12 +17,11 @@ color ray_color(const ray &r, const hittable &world, int depth) {
     }
 
     if (world.hit(r, 0.001, infinity, rec)) {
-#if LAMBERTIAN
-        point3 target = rec.p + rec.normal + random_unit_vector(); // Lambertian scattering
-#else
-        point3 target = rec.p + random_in_hemisphere(rec.normal); // Hemispherical scattering
-#endif
-        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
+        ray scattered;
+        color attenuation;
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+            return attenuation * ray_color(scattered, world, depth - 1);
+        return color(0, 0, 0);
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
@@ -41,8 +39,16 @@ int main() {
 
     // World
     hittable_list world;
-    world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
-    world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+    auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = std::make_shared<lambertian>(color(0.7, 0.3, 0.3));
+    auto material_left   = std::make_shared<metal>(color(0.8, 0.8, 0.8));
+    auto material_right  = std::make_shared<metal>(color(0.8, 0.6, 0.2));
+
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
 
     // Camera
 
--- material.h
+++ material.h
@@ -38,6 +38,8 @@ public:
     color albedo;
 
 public:
+    metal(const color &a) : albedo(a) {}
+
     virtual bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered) const override {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
         scattered = ray(rec.p, reflected);
