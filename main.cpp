2f56964eabeca9cb1571e49bb2f06db026ecf6a8
Trevor <trevorllew@simpleworks.tech>
2023-02-08 16:15:22 -0500

    Added fuzzy reflection

--- main.cpp
+++ main.cpp
@@ -42,8 +42,8 @@ int main() {
 
     auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = std::make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left   = std::make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right  = std::make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left   = std::make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_right  = std::make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
